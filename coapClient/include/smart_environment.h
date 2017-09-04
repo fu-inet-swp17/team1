9d6333ab48cd5135e7e3283de404d49c7afbbb71
phjort <patrick.hjort@yahoo.de>
2017-09-04 13:47:28 +0200

    merged client updates into master branch

---
+++ coapClient/include/smart_environment.h
@@ -0,0 +1,10 @@
+#ifndef _SMART_ENVIRONMENT_H_
+#define _SMART_ENVIRONMENT_H_
+
+#define SERVER_CONN_PORT  2017
+
+// statische globals, nach außen hin nicht sichtbar
+static const char app_id[] = "riot-swp-2017-se";
+#define APP_ID_LEN      sizeof(app_id)
+
+#endif
--- coapClient/main.c
+++ coapClient/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "msg.h"
+#include "shell.h"
+
+#include "net/ipv6/addr.h"
+#include "net/sock/udp.h"
+#include "net/gnrc/ipv6/netif.h"
+#include "saul.h"
+#include "saul_reg.h"
 
 #include "thread.h"
 #include "net/gcoap.h"
 #include "kernel_types.h"
+#include "coap.h"
+#define MAIN_QUEUE_SIZE (8)
 
-#define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
-char stack[THREAD_STACKSIZE_MAIN];
+char coap_client_thread_stack[THREAD_STACKSIZE_DEFAULT];
+char own_addr[IPV6_ADDR_MAX_STR_LEN];
 
 extern int gcoap_cli_cmd(int argc, char **argv);
 
@@ -27,6 +36,10 @@ static char * M1_result = "";
 static int M0_rcvd_result = 0;
 static int M1_rcvd_result = 0;
 static int restart_counter = 0;
+
+int entry_counter = 0;
+char * nameslist[50];
+int resultslist[50];
 
 void set_name(char * name, int m) {
     if (m == 0){
@@ -114,16 +127,16 @@ void *thread_handler(void *arg) {
                         gcoap_cli_cmd(5, message1);
                         xtimer_sleep(5);
                         /*if (atol(M0_result) < atol(M1_result)) {
-                            char * message1[] = {"coap", "get", M0_ADDR, "5683", "/print/looser"};
+                            char * message1[] = {"coap", "get", M0_ADDR, "5683", "/set/winner"};
                             gcoap_cli_cmd(5, message1);
-                            char * message2[] = {"coap", "get", M1_ADDR, "5683", "/print/winner"};
+                            char * message2[] = {"coap", "get", M1_ADDR, "5683", "/set/looser"};
                             gcoap_cli_cmd(5, message2);
                             //send result to Database
                         }
                         else if (atol(M0_result) > atol(M1_result)) {
-                            char * message1[] = {"coap", "get", M0_ADDR, "5683", "/print/looser"};
+                            char * message1[] = {"coap", "get", M0_ADDR, "5683", "/set/looser"};
                             gcoap_cli_cmd(5, message1);
-                            char * message2[] = {"coap", "get", M1_ADDR, "5683", "/print/winner"};
+                            char * message2[] = {"coap", "get", M1_ADDR, "5683", "/set/winner"};
                             gcoap_cli_cmd(5, message2);
                             //send result to Database
                         }
@@ -134,6 +147,16 @@ void *thread_handler(void *arg) {
 
                         printf("\n\nResetting machine status\n\n\n");
                         
+                        nameslist[entry_counter] = M0_name;
+                        nameslist[entry_counter+1] = M1_name;
+                        resultslist[entry_counter] = atoi(M0_result);
+                        resultslist[entry_counter+1] = atoi(M1_result);
+                        entry_counter += 2;
+
+                        if (entry_counter >= 48){
+                            entry_counter = 0;
+                        }
+
                         M0_name = "";
                         M1_name = "";
                         M0_has_name = 0;
@@ -160,7 +183,29 @@ int main(void) {
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("gcoap client app fuer Bandgeraete Spiel");
 
-    thread_create(stack, sizeof(stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, thread_handler, NULL, "main_thread");
+    ipv6_addr_t addr;
+    ipv6_addr_from_str(&addr, "fe80::");
+
+    ipv6_addr_t* out = NULL;
+    gnrc_ipv6_netif_find_by_prefix(&out, &addr);
+    
+    ipv6_addr_to_str(own_addr, out, IPV6_ADDR_MAX_STR_LEN);
+    printf("own ipv6 ll-addr: %s\n", own_addr);
+
+    gcoap_register_listener(&coap_listener);
+    puts("coap ready");
+
+    // thread für pings
+    thread_create(ping_stack, THREAD_STACKSIZE_DEFAULT, THREAD_PRIORITY_MAIN - 1, 
+        THREAD_CREATE_STACKTEST, ping_handler, NULL, "ping");
+
+    thread_create(coap_client_thread_stack, sizeof(coap_client_thread_stack), THREAD_PRIORITY_MAIN - 1, 
+        THREAD_CREATE_STACKTEST, thread_handler, NULL, "client_thread");
+
+    puts("Main: Starting shell.");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    puts("Main: Shell done.");
 
     return 0;
 }
